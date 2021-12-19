563915018523f325f9efcb24f0ee59849d270532
Pandi <pandi@wavetogether.com>
2021-12-19 20:55:48 +0900

    firebase 1.0

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAgQiplhpEWPfeq3jhp64gzLx6jT4GQ3ss",
+  authDomain: "nwitter-46e5c.firebaseapp.com",
+  projectId: "nwitter-46e5c",
+  storageBucket: "nwitter-46e5c.appspot.com",
+  messagingSenderId: "137047499733",
+  appId: "1:137047499733:web:24f30ed045d25f6655535c",
+  measurementId: "G-2F2650HL1N",
+};
+export default firebase.initializeApp(firebaseConfig);
