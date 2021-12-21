1338e3940ebc2b0ca2eb0b96768862cc17c52e84
Pandi <pandi@wavetogether.com>
2021-12-21 12:43:15 +0900

    image 4.0

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -20,7 +20,7 @@ const Home = ({ userObj }) => {
     event.preventDefault();
     await dbService.collection("nweets").add({
       text: nweet,
-      createdAt: Date.now(),
+      createdAt: Date.now() /*데이터 저장위치 지정 */,
       creatorId: userObj.uid,
     });
     setNweet("");
@@ -33,6 +33,18 @@ const Home = ({ userObj }) => {
     setNweet(value);
   };
 
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      console.log(finishedEvent);
+    };
+    reader.readAsDataURL(theFile);
+  };
+
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -43,6 +55,7 @@ const Home = ({ userObj }) => {
           placeholder="What's on your mind?"
           maxLength={120}
         />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Nweet" />
       </form>
       <div>
